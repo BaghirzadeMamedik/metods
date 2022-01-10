2ed21e9c417ac2bdf5348d66779da5cdd850fa6b
BaghirzadeMamedik <brizeforge@gmail.com>
2022-01-10 13:59:09 +0400

    Add project files.

---
+++ metods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Initial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Mamedik";
+
+            //test.GetHashCode();
+            Console.WriteLine(test.GetTypeCode());
+            Console.WriteLine(test.GetHashCode());
+
+            int[] Num = { 10, 20, 30, 40 };
+
+            Console.WriteLine(Num.Max());
+            Console.WriteLine(Num.Min());
+        }
+    }
+}
